edfc4356f86bb2c75e5069e2d2c3c6e7e6ddca3b
Tadas Gricius <griciustadas@gmail.com>
2020-12-28 23:31:24 +0200

    navigation bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-console.log('veikia')
+const body = document.querySelector('body')
+const burgerBtn = document.querySelector('.js-burger-menu')
+const nav = document.querySelector('.js-nav')
+
+burgerBtn.addEventListener('click', () => {
+  body.classList.toggle('disable')
+  nav.classList.toggle('active')
+  burgerBtn.classList.toggle('active')
+})
